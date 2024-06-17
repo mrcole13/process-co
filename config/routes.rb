5c60603d0f151be3e6ab06252b31167b10064fd6
Matt Cole <mattcole@Matts-MacBook-Pro.local>
2024-06-17 15:46:24 -0500

    webhook routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     end
   end
   resources :webhooks, only: [:create]
+  namespace :connect do
+    resources :webhooks, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
